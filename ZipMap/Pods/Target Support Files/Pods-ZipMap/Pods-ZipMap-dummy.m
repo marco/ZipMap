22e28f3e79b08f25e18bcb3c7eef3703adee2154
skunkmb <mxq900@gmail.com>
2016-06-19 16:36:47 -0700

    Installed Firebase by CocoaPods
    Firebase’s code has now been added to the project, a new .xcworkspace
    has been created, and FIRApp.configure() has been called in
    AppDelegate.swift .

---
+++ ZipMap/Pods/Target Support Files/Pods-ZipMap/Pods-ZipMap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZipMap : NSObject
+@end
+@implementation PodsDummy_Pods_ZipMap
+@end
---
+++ ZipMap/ZipMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZipMap
+//
+//  Created by Marco Burstein on 6/19/16.
+//  Copyright © 2016 Skunk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
