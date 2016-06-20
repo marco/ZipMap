267983bb85d8b03c1c9c58e2ea2ca5bd939b6a70
skunkmb <mxq900@gmail.com>
2016-06-20 16:53:55 -0700

    Added UI for part of the Sign Up screen
    There are now name, birthday, and username and email pages on the Sign
    Up page. They all have buttons that are only interactive when the
    entries follow the guidelines.

--- ZipMap/ZipMap/SignUpViewController.swift
+++ ZipMap/ZipMap/SignUpViewController.swift
@@ -9,19 +9,76 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+	
+	@IBOutlet var nameView: UIView!
+	@IBOutlet var birthView: UIView!
+	@IBOutlet var usernameEmailView: UIView!
 
+	@IBOutlet var firstNameField: UITextField!
+	@IBOutlet var lastNameField: UITextField!
+	@IBOutlet var nameNextButton: UIButton!
+	
+	@IBOutlet var birthPicker: UIDatePicker!
+	@IBOutlet var birthNextButton: UIButton!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		setUpNameView()
+		setUpBirthView()
+		// Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+	@IBAction func nameFieldUpdated(sender: AnyObject) {
+		if(firstNameField.text != "" || lastNameField.text != ""){
+			nameNextButton.enabled = true
+		}
+		else{
+			nameNextButton.enabled = false
+		}
+	}
+	@IBAction func nameNextPressed(sender: AnyObject) {
+		self.view = birthView
+	}
+	@IBAction func birthFieldUpdated(sender: AnyObject) {
+		let minYearsAway = 13
+		//current by default
+		let currentDate = NSDate()
+		let maximumYear = NSCalendar.currentCalendar().component(NSCalendarUnit.Year, fromDate: currentDate) - minYearsAway
+		let selectedYear = NSCalendar.currentCalendar().component(NSCalendarUnit.Year, fromDate: birthPicker.date)
+		if(selectedYear <= maximumYear){
+			birthNextButton.enabled = true
+		}
+		else{
+			birthNextButton.enabled = false
+		}
+	}
+	@IBAction func birthNextPressed(sender: AnyObject){
+		self.view = usernameEmailView
+	}
+	
+	
+	
+	func setUpNameView(){
+		nameNextButton.setBackgroundImage(UIImage(named: "buttonBackground"), forState: UIControlState.Normal)
+		nameNextButton.setBackgroundImage(UIImage(named: "buttonClickedBackground"), forState: UIControlState.Highlighted)
+		nameNextButton.setBackgroundImage(UIImage(named: "buttonDisabledBackground"), forState: UIControlState.Disabled)
+		
+		nameNextButton.enabled = false
+	}
+	func setUpBirthView(){
+		birthNextButton.setBackgroundImage(UIImage(named: "buttonBackground"), forState: UIControlState.Normal)
+		birthNextButton.setBackgroundImage(UIImage(named: "buttonClickedBackground"), forState: UIControlState.Highlighted)
+		birthNextButton.setBackgroundImage(UIImage(named: "buttonDisabledBackground"), forState: UIControlState.Disabled)
+		
+		birthNextButton.enabled = false
+	}
 
+	
     /*
     // MARK: - Navigation
 
